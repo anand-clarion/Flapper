3e9fb51769a5584eaf1ffebb547d51734bc76e3c
Anand Tripathi <anand@Anand-Tripathi.(none)>
2015-10-13 14:52:18 +0530

    Added inceptor for 401 Error handling.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,45 @@
 var app = angular.module("myApp", ['ngRoute', 'ngCookies',  'templates', 'home',
-                         'posts', 'Devise', 'auth' ]);
+                         'posts', 'Devise', 'auth', 'ngMessages']);
 
 
-app.run(function($cookieStore, $rootScope) {
-  $rootScope.isSignedIn = $cookieStore.get('loggedin');
+// To Handle the unauthorized 401 Error.
+app.factory('authHttpResponseInterceptor',function($q,$location){
+  return {
+    response: function(response){
+      if (response.status === 401) {
+          console.log("Response 401");
+      }
+      return response || $q.when(response);
+    },
+    responseError: function(rejection) {
+      if (rejection.status === 401) {
+          console.log("Response Error 401 we handeled it successfully",rejection);
+          if($location.path() == "/register") {
+            console.clear();
+          }
+          else {
+            $location.path('/login')
+          }
+      }
+      return $q.reject(rejection);
+    }
+  }
 })
 
-app.config(function($routeProvider){
+// Re initialized session on page refresh
+app.run(function($cookieStore, $rootScope, Auth) {
+  Auth.currentUser().then(function(user) {
+      $rootScope.isSignedIn = true
+    }, function(error) {
+      $rootScope.isSignedIn = false
+  });
+  $rootScope.errors = [];
+})
 
+// Define routes and app configuration.
+app.config(function($routeProvider, $httpProvider){
+
+  $httpProvider.interceptors.push('authHttpResponseInterceptor');
   $routeProvider
     .when('/home', {
       templateUrl: 'home/_home.html',
@@ -17,7 +49,7 @@ app.config(function($routeProvider){
       templateUrl: "auth/_login.html",
       controller: "AuthCtrl",
       onEnter: function($location, Auth) {
-        Auth.currentUser().then(function(user) {
+        Auth.currentUser().then(function() {
           $location.path('/posts')
         })
       }
@@ -28,7 +60,12 @@ app.config(function($routeProvider){
     })
     .when('/posts', {
       templateUrl: "posts/_post_list.html",
-      controller: "PostCtrl"
+      controller: "PostCtrl",
+      onEnter: function($location, Auth) {
+        Auth.currentUser().then(function() {
+          $location.path('/posts')
+        })
+      }
     })
     .when('/posts/:id', {
       templateUrl: "posts/_post_show.html",
