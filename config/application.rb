8d3460590720af627415db313814da8084b43f0a
Anand tripathi <anand_tripathi@ymail.com>
2016-02-25 10:12:40 +0530

    Updated the assets path configuration.

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Flapper
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_controller.include_all_helpers = true
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
   end
 end
